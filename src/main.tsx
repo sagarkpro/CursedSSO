bbee44e596a1c6ad8d5a19eb24d6611dc209d219
Sagar Khatri <sagarkhatri.pro@gmail.com>
2026-05-31 01:59:57 +0530

    sonner in topright

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Toaster } from 'sonner'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import "./index.css";
+import App from "./App.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster richColors theme="dark" />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+			<Toaster richColors theme="dark" position="top-right" />
+		</QueryClientProvider>
+	</StrictMode>,
+);
